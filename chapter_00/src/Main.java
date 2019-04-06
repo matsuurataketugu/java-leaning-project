584b3b91aaf2e865e0d88e97fa0a0221f5c8d759
matsuura <gf12a070@oecu.jp>
2019-04-06 23:15:05 +0900

    Add: 社員番号追加

--- chapter_00/src/Main.java
+++ chapter_00/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello 092 san!");
-        System.out.println("Hello 000 san!");
+        System.out.println("Hello 645 san!");
     }
 }
