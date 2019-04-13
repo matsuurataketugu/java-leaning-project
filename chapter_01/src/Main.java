e3ebc687c45d564da4f4d23c4011ed01fc0389a8
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-04-14 00:30:31 +0900

    Add: 課題１を追加

--- chapter_01/src/Main.java
+++ chapter_01/src/Main.java
@@ -1,5 +1,7 @@
+import controller.MainController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        MainController.run();
     }
 }
---
+++ chapter_01/src/domain/service/TaskServiceImplementedBy660.java
@@ -0,0 +1,26 @@
+package domain.service;
+
+/**
+ * 課題実装部
+ */
+public class TaskServiceImplementedBy660 implements ITask {
+    // 問１
+    @Override
+    public void learnJavaMechanism() {
+    }
+
+    // 問２
+    @Override
+    public void learnVariable() {
+    }
+
+    // 問３
+    @Override
+    public void learnConstant() {
+    }
+
+    // 問４
+    @Override
+    public void learnDataType() {
+    }
+}
