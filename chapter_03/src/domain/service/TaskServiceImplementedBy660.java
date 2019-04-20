2a9f1874fc76062bca51c538647b3966a1f62c4a
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-04-20 12:12:19 +0900

    Add: ３章課題を追加

---
+++ chapter_03/src/domain/service/TaskServiceImplementedBy660.java
@@ -0,0 +1,45 @@
+package domain.service;
+
+public class TaskServiceImplementedBy660 implements ITask {
+    @Override
+    public void learnIfSyntax1() {
+
+    }
+
+    @Override
+    public void learnIfSyntax2() {
+
+    }
+
+    @Override
+    public void learnIfStatement1() {
+
+    }
+
+    // 問４
+    // 下記のような実装が行われている
+    // weatherがfalseの場合、「家でゲーム」の後に「寝る」と表示
+    // のつもりで書いたが意図したように動かない
+    // 正しく動くように修正せよ
+    @Override
+    public void learnIfStatement2() {
+        System.out.print("今日の天気は？(0:晴れ 1:くもり 2:雨) > ");
+        boolean weather = new java.util.Scanner(System.in).nextInt() == 0;
+        if (weather) {
+            System.out.println("洗濯をする");
+            System.out.println("彼女or彼氏とデート");
+        } else
+            System.out.println("家でゲーム");
+        System.out.println("寝る");
+    }
+
+    @Override
+    public void learnSwitchStatement() {
+
+    }
+
+    @Override
+    public void countGame() {
+
+    }
+}
