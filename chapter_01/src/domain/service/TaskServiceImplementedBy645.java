eb636be3fbf37d4d5ac5821285e0a904b031c3b9
kuturogi99with99jazz <kuturogi99with99jazz@yahoo.co.jp>
2019-04-27 00:11:37 +0900

    Update TaskServiceImplementedBy645.java
    大体あっていますが、変数名を修正しました。

--- chapter_01/src/domain/service/TaskServiceImplementedBy645.java
+++ chapter_01/src/domain/service/TaskServiceImplementedBy645.java
@@ -11,8 +11,6 @@ public class TaskServiceImplementedBy645 implements ITask {
         System.out.println("Java仮想マシンと呼ばれるソフトウェア上で実行される。");
         System.out.println("各コンピュータ環境に対応したJava仮想マシンがハードウェア間の差異を吸収し、");
         System.out.println("特定の環境に依存しないプログラム動作を実現する仕組みとなっている。");
-
-
     }
 
     // 問２
@@ -22,33 +20,29 @@ public class TaskServiceImplementedBy645 implements ITask {
         int y = 5 ;
         int z = x*y;
         System.out.println("縦幅" + x + "横幅 "+ y +" の長方形の面積は、"+ z );
-
     }
 
     // 問３
     @Override
     public void learnConstant() {
-
-        double TAX = 1.08;
-        int a = 100;
-        double b = a * TAX ;
-        System.out.println("商品お買い上げありがとうございます。"+ (int)b +"円になります");
+        final double TAX = 1.08;
+        int price = 100;
+        double cost = price * TAX ;
+        System.out.println("商品お買い上げありがとうございます。"+ (int)cost +"円になります");
     }
 
     // 問４
     @Override
     public void learnDataType() {
-        boolean c = true;
-        char d = '侍';
-        double e = 3.14;
-        long f = 314159265853979L;
-        String g = "為せば成る 為さねば成らぬ 何事も 成らぬは人の 為さぬなりけり";
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-
-
+        boolean flag = true;
+        char character = '侍';
+        double pi = 3.14;
+        long largeValue = 314159265853979L;
+        String saying = "為せば成る 為さねば成らぬ 何事も 成らぬは人の 為さぬなりけり";
+        System.out.println(flag);
+        System.out.println(character);
+        System.out.println(pi);
+        System.out.println(largeValue);
+        System.out.println(saying);
     }
 }
