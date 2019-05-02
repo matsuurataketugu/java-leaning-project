6d0e7afcca2bdd551d60bfbdf0e0027afd9cd9b7
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-05-02 13:00:53 +0900

    Add: ６章課題を追加

---
+++ chapter_06/src/domain/service/PaladinBy092.java
@@ -0,0 +1,19 @@
+package domain.service;
+
+public class PaladinBy092 {
+    public String name = "nanashi";
+    public String life = "0";
+    public String mana = "0";
+    public String max_life = "0";
+    public String max_mana = "0";
+
+    public void zeal() {
+        System.out.println("zeal! zeal! zeal!");
+    }
+    public void fanaticism() {
+        System.out.println("fanaticism (^0^)/");
+    }
+    public void meditation() {
+        System.out.println("meditation...");
+    }
+}
