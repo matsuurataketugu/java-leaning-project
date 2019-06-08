3d0512bda177799acb96a8622e60cdd4a3250621
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-06-08 17:24:41 +0900

    Add:chapter_02の課題を実施しました。

--- chapter_02/src/domain/service/TaskServiceImplementedBy092.java
+++ chapter_02/src/domain/service/TaskServiceImplementedBy092.java
@@ -1,23 +1,74 @@
 package domain.service;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TaskServiceImplementedBy092 implements ITask {
+    // 問１
+    // 現在すでに処理は実装されている。
+    // しかし、動作させると「x + y = 23」と表示されてしまう。
+    // 「x + y = 5」と表示されるように修正せよ。
     @Override
     public void learnOperatorPriority() {
         int x = 2;
         int y = 3;
-        String answer = "x + y = " + x + y;
+        String answer = "x + y = " + (x + y);
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         System.out.println(answer);
     }
 
+    // 問２
+    // 次の中で文法として正しいものをすべて表示せよ。
+    // 回答例. 「正しい文は①, ②, ③です。」
+    // ①int x = 1 + 2.0;
+    // ②double d = 4.0F:
+    // ③int i = "9";
+    // ④String s = 3 + " days";
+    // ⑤byte b = 7;
+    // ⑥double d = true;
+    // ⑦short s = (byte)2;
     @Override
     public void learnTypeConversion() {
-
+        System.out.println("正しい文は②, ④, ⑤, ⑦です。");
     }
 
+    // 問３
+    // 以下の内容のプログラムを作成せよ。
+    // 1. 画面に「ようこそ占いの館へ」と表示
+    // 2. 画面に「あなたの名前を入力してください」と表示
+    // 3. キーボードから１行の文字入力を受け付け、String型の変数nameに格納
+    // 4. 画面に「あなたの年齢を入力してください」と表示
+    // 5. キーボードから１行の文字入力を受け付け、String型の変数ageStringに格納
+    // 6. 変数ageStringの内容をint型に変換し、int型の変数ageに代入
+    // 7. ０から３までの乱数を生成し、int型の変数fortuneに代入
+    // 8. fortuneの数値をインクリメント演算子で１増やし、１から４の乱数する
+    // 9. 画面に「占いの結果が出ました！」と表示
+    // 10. 画面に「(年齢)歳の(名前)さん、あなたの運気番号は(乱数)です。」と表示 ※カッコの中身は変数
+    // 11. 画面に「1:大吉 2:中吉 3:吉 4:凶」と表示
+    // ヒント：
+    //   文字入力受付  java.util.Scanner
+    //   ランダム生成  java.util.Random
     @Override
     public void fortuneGame() {
-
+        System.out.println("============================================");
+        System.out.println("==           ようこそ占いの館へ           ==");
+        System.out.println("============================================");
+        System.out.print("あなたの名前を入力してください。 > ");
+        String name = new Scanner(System.in).next();
+        System.out.print("あなたの年齢を入力してください。 > ");
+        String ageString = new Scanner(System.in).next();
+        int age;
+        try {
+            age = Integer.parseInt(ageString);
+        } catch (NumberFormatException e) {
+            System.out.println("整数で入力してください。");
+            return;
+        }
+        int fortune = new Random().nextInt(4);
+        fortune++;
+        System.out.println("占いの結果が出ました！");
+        System.out.println(age + "歳の" + name + "さん、あなたの運気番号は(" + fortune + ")です。");
+        System.out.println("「1:大吉 2:中吉 3:吉 4:凶」");
     }
 }
