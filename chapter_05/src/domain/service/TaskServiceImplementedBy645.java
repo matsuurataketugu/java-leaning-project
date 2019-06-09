0245d65c0b580cd0eb573c97c1981602705752a0
matsuura <gf12a070@oecu.jp>
2019-06-09 22:31:03 +0900

    Add: 課題5　指摘修正

--- chapter_05/src/domain/service/TaskServiceImplementedBy645.java
+++ chapter_05/src/domain/service/TaskServiceImplementedBy645.java
@@ -13,36 +13,38 @@ public class TaskServiceImplementedBy645 implements ITask {
     private void selfIntroduction() {
         //引数なし　→　selfIntroduction()
         //返り値無し　→　void
-        //エラーフラグが真でエラー
 
-        boolean errorflg = false;
         System.out.print("お名前は？＞");
         String name = new Scanner(System.in).next();
 
         System.out.print("年齢は？＞");
-        int old = new Scanner(System.in).nextInt();
-        if (old == 0) {
-            errorflg = true;
+        Scanner scanold = new Scanner(System.in);
+        //int old = 0;
+        if(scanold.hasNextInt()){
+        }else{
+            System.out.println("エラー");
+            return;
         }
+        int old = scanold.nextInt();
 
         System.out.print("伸長は何センチ？＞");
-        double sintyou = new Scanner(System.in).nextInt();
-        if (sintyou == 0) {
-            errorflg = true;
+        Scanner scansintyou = new Scanner(System.in);
+        if(scansintyou.hasNextDouble()){
+        }else{
+            System.out.println("エラー");
+            return;
         }
+        double sintyou = scansintyou.nextDouble();
 
         System.out.print("性別は？男or女＞");
         String sex = new Scanner(System.in).next();
         if (sex.equals("男")||sex.equals("女")) {
         }else{
-            errorflg = true;
+            System.out.println("エラー");
+            return;
         }
+            System.out.println( old + "歳の" + name + "さんは、伸長が" + sintyou + "の" + sex + "なんですね！");
 
-        if (errorflg) {
-            System.out.println("エラー!どこか間違った入力あり!");
-        } else {
-            System.out.println(old + "歳の" + name + "さんは、伸長が" + sintyou + "の" + sex + "なんですね！");
-        }
 
     }
 
@@ -50,7 +52,7 @@ public class TaskServiceImplementedBy645 implements ITask {
     public void learnMethod2() {
         System.out.print("九九を行います！数字を入力してください＞");
         int kuku = new Scanner(System.in).nextInt();
-        if (kuku > 10) {
+        if ( kuku == 0 || kuku > 10) {
             System.out.println("九九は１から９しかできません！！");
         }else {
             multiplication(kuku);
@@ -58,9 +60,9 @@ public class TaskServiceImplementedBy645 implements ITask {
         }
 
     private void multiplication(int kakezan){
-        int  kukuList[] = new int[10];
-        for(int i = 0; i<=9; i++){
-            kukuList[i] = kakezan * i;
+        int  kukuList[] = new int[9];
+        for(int i = 0; i<9; i++){
+            kukuList[i] = kakezan * (i+1);
         }
         System.out.println(kakezan + "の段は↓");
         for(int j : kukuList ){
@@ -97,61 +99,98 @@ public class TaskServiceImplementedBy645 implements ITask {
 
             System.out.print("西暦と価格を入力してください！その年の消費税を答えます！");
             System.out.print("西暦は？＞");
-            int seireki = new Scanner(System.in).nextInt();
+            Scanner scanseireki = new Scanner(System.in);
+            if(scanseireki.hasNextInt()){
+            }else{
+                System.out.println("エラー");
+                return;
+            }
+            int seireki = scanseireki.nextInt();
 
             System.out.print("価格は？＞");
-            int price = new Scanner(System.in).nextInt();
+            Scanner scanprice = new Scanner(System.in);
+            //int old = 0;
+            if(scanprice.hasNextInt()){
+            }else{
+                System.out.println("エラー");
+                return;
+            }
+            int price = scanprice.nextInt();
 
             taxCalculation(seireki, price);
         }
 
     private void taxCalculation(int nen,int pri) {
-        boolean errorflg1 = false;
-        boolean errorflg2 = false;
+
+        //税のパターン
+        int taxpattterm;
+
+        //消費税込み価格
         int taxprice;
+
+        //消費税
         double tax = 0;
 
         //入力値チェック
-        if (nen <= 2020 && nen >= 1900){
-            errorflg1 = true;
+        if (nen >= 2020 && nen <= 1900){
+            System.out.println("エラー");
+            return;
         }
-        if (pri != 0 ){
-            errorflg2 = true;
+        if (pri == 0 ){
+            System.out.println("エラー");
+            return;
         }
 
-        //西暦の判定
-        if(errorflg1) {
-            if(nen <= 2020) {
-                if(nen <= 1997) {
-                    if(nen <= 1989) {
-                        if(nen <=1988) {
-                        }
-                    tax = 0.03;
-                    }
-                tax = 0.05;
-                }
-            tax = 0.1;
-            }
+        //消費税パターンの判定
+        if(nen < 1989){
+            taxpattterm = 1;
+        }else if(nen < 1997){
+            taxpattterm = 2;
+        }else if(nen < 2020){
+            taxpattterm = 3;
         }else {
-            System.out.println("西暦が不正！！");
+            taxpattterm = 4;
         }
-
+        switch (taxpattterm){
+            case 1:
+                tax = 0;
+                break;
+            case 2:
+                tax = 0.03;
+                break;
+            case 3:
+                tax = 0.05;
+                break;
+            case 4:
+                tax = 0.1;
+                break;
+        }
         taxprice = pri + (int)(tax * pri);
-        if (errorflg2) {
-            System.out.print(nen + "年の場合、消費税は"+ ( tax * 100) +"％！税込み価格は"+ taxprice);
-        }else{
-            System.out.println("価格が不正！！");
-        }
+        System.out.print(nen + "年の場合、消費税は"+ ( tax * 100) +"％！税込み価格は"+ taxprice);
      }
 
         @Override
         public void learnMethod5 () {
             System.out.print("君の名は何回見た？＞");
-            int kaisu = new Scanner(System.in).nextInt();
+            Scanner scankaisu = new Scanner(System.in);
+            if(scankaisu.hasNextInt()){
+            }else{
+                System.out.println("エラー");
+                return;
+            }
+            int kaisu = scankaisu.nextInt();
             yourName(kaisu);
 
             System.out.print("君の名は？＞");
-            String name = new Scanner(System.in).next();
+           // String name = new Scanner(System.in).next();
+            Scanner scanname = new Scanner(System.in);
+            if(scanname.hasNext()){
+            }else{
+                System.out.println
+                        ("エラー");
+                return;
+            }
+            String name = scanname.next();
             yourName(name);
 
         }
