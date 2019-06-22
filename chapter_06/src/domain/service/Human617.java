683b8fa8921a50000619f2eb510eac50453890fc
aiwa12 <AIWA522@ouotlook.jp>
2019-06-22 23:58:02 +0900

    Add:chapter_06の課題を実施

---
+++ chapter_06/src/domain/service/Human617.java
@@ -0,0 +1,20 @@
+package domain.service;
+
+public class Human617 {
+
+    public String name = "渡邉 愛";
+    public int age =  26;
+    public String sex = "女";
+
+    public void selfIntroduction() {
+
+        System.out.println("自己紹介をします。");
+        System.out.println("私の名前は" + name + "です。");
+        System.out.println("年齢は" + age + "歳です。");
+        System.out.println("性別は" + sex + "です。");
+        System.out.println("将来の夢はハワイに住むことです。");
+    }
+
+}
+
+
