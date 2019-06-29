3b7563bab5f74802ae98f8169447d05db7b2138a
matsuura <gf12a070@oecu.jp>
2019-06-29 10:33:39 +0900

    Add: 課題6　完成

--- chapter_06/src/domain/service/Human645.java
+++ chapter_06/src/domain/service/Human645.java
@@ -1,13 +1,15 @@
 package domain.service;
 
 public class Human645 {
-    String name = "松浦　武嗣";
+    String name = "松浦 武嗣";
     int old = 25;
     String sex = "man";
-    private void selfIntroduction(){
+
+
+    public void selfIntroduction(){
         System.out.println("私は" + name + "で,"+ old + "歳の、"+ sex + "です。");
         System.out.println("犬や猫が好きで、猫カフェによく行きます");
-        System.out.println("私の将来の夢は、家族の居ない犬猫を家族に向かい入れて、");
+        System.out.println("私の将来の夢は、家族の居ない犬猫を家族に迎い入れて、");
         System.out.println("犬猫に囲まれた生活を送りたいです。");
     }
 
