f965215fc8ed46f90ffc0ebfb1f6e81e95556a5e
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-09-13 17:05:22 +0900

    Add: 課題１２を作成

---
+++ chapter_12/src/domain/service/student092/Task092.java
@@ -0,0 +1,15 @@
+package domain.service.student092;
+
+import domain.service.ITask;
+
+public class Task092 implements ITask {
+    @Override
+    public void task1() {
+
+    }
+
+    @Override
+    public void task2() {
+
+    }
+}
