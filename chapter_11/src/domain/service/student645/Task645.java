cca03d4d5a1b06c01f99638b6be9129d9f2c07cc
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-09-13 16:07:16 +0900

    Add: 課題１１を作成

---
+++ chapter_11/src/domain/service/student645/Task645.java
@@ -0,0 +1,25 @@
+package domain.service.student645;
+
+import domain.service.ITask;
+
+public class Task645 implements ITask {
+    @Override
+    public void task1() {
+
+    }
+
+    @Override
+    public void task2() {
+
+    }
+
+    @Override
+    public void task3() {
+
+    }
+
+    @Override
+    public void task4() {
+
+    }
+}
