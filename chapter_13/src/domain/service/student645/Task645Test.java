b0980c5e355863406f64760cf4cc86d34e90089b
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-09-14 11:12:18 +0900

    Fix: 課題１３のテストを修正

--- chapter_13/src/domain/service/student645/Task645Test.java
+++ chapter_13/src/domain/service/student645/Task645Test.java
@@ -7,20 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class Task645Test {
 
     @Test
-    void task1() {
+    void すべての文字列が正常となる() {
         ITask task = new Task645();
         assertTrue(task.task1("すべての文字列"));
     }
 
     @Test
-    void task2() {
+    void 最初の１文字はＡで２文字目は数字で３文字目は数字か無しが正常となる() {
         ITask task = new Task645();
         assertTrue(task.task1("A10"));
     }
 
     @Test
-    void task3() {
+    void 最初の１文字はＵで２文字目から４文字目は英大文字が正常となる() {
         ITask task = new Task645();
-        assertTrue(task.task1("USA"));
+        assertTrue(task.task1("USAGI"));
     }
 }
