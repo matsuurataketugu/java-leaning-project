cc4c7524998e3560cbf8c411731d8a0c9574fd73
horii <5711jyyd@jcom.zaq.ne.jp>
2019-10-05 14:19:44 +0900

    Add:#143の課題を実施

--- chapter_11/src/domain/service/student506/Task506.java
+++ chapter_11/src/domain/service/student506/Task506.java
@@ -2,24 +2,50 @@ package domain.service.student506;
 
 import domain.service.ITask;
 
+import java.io.File;
+import java.io.FileReader;
+
 public class Task506 implements ITask {
     @Override
     public void task1() {
+
+        String s = null;
+        System.out.println(s.length());
 
     }
 
     @Override
     public void task2() {
 
+        String s = null;
+
+        try {
+            System.out.println(s.length());
+        } catch(Exception e) {
+            System.out.println("－－スタックトレース（ここから）－－");
+            e.printStackTrace();
+            System.out.println("－－スタックトレース（ここまで）－－");
+        }
     }
 
     @Override
     public void task3() {
 
+        String s = "3";
+        int number = 0;
+
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void task4() {
+
+        File inputFile = new File( "あいうえお" );
 
     }
 }
