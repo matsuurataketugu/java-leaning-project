d52f0ac5a77eaca05ad197bd45e4ec03a46e91e5
horii <5711jyyd@jcom.zaq.ne.jp>
2019-10-20 21:50:43 +0900

    Add:#155の課題を実施

--- chapter_13/src/domain/service/student506/Task506.java
+++ chapter_13/src/domain/service/student506/Task506.java
@@ -2,19 +2,47 @@ package domain.service.student506;
 
 import domain.service.ITask;
 
+import java.util.regex.Pattern;
+
 public class Task506 implements ITask {
     @Override
     public boolean task1(String s) {
-        return false;
+
+        return s.matches(".*");
+
+        //return false;
     }
 
     @Override
     public boolean task2(String s) {
+
+        String pattern = "^A[0-9]{2}";
+        Pattern p = Pattern.compile(pattern);
+
+        String pattern2 = "^A[0-9]";
+        Pattern p2 = Pattern.compile(pattern);
+
+        if(p.matcher(s).find()){
+            return true;
+        }
+
+        if(p2.matcher(s).find()){
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean task3(String s) {
+
+        String pattern = "^U[A-Z]{3}";
+        Pattern p = Pattern.compile(pattern);
+
+        if(p.matcher(s).find()){
+            return true;
+        }
+
         return false;
     }
 }
