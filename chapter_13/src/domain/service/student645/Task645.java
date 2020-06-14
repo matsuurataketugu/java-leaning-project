b34ca10dec51944688d941661070202b83c4eca8
matsuura <gf12a070@oecu.jp>
2020-06-14 19:42:45 +0900

    課題１3 作業中

--- chapter_13/src/domain/service/student645/Task645.java
+++ chapter_13/src/domain/service/student645/Task645.java
@@ -2,22 +2,37 @@ package domain.service.student645;
 
 import domain.service.ITask;
 
+import java.util.regex.Pattern;
+
 public class Task645 implements ITask {
     @Override
     public boolean task1(String s) {
-        matches('s');
-        return false;
+
+        return s.matches( ".*");
+
     }
 
     @Override
     public boolean task2(String s) {
 
+        String moziA = "^A[0-9]{1,2}$'";
+        Pattern pat = Pattern.compile(moziA);
+
+        if (pat.matcher(s).find()) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean task3(String s) {
 
-        return false;
-    }
+            String moziB = "^U[A-Z]{3}$'";
+            Pattern patB = Pattern.compile(moziB);
+            if (patB.matcher(s).find()) {
+                return true;
+            }
+            return false;
+        }
+
 }
